9f3e5cf036d25c19bef62b656a5ad9d5910be806
unknown <yc0519165>
2024-12-22 20:52:32 +0530

    update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -95,6 +95,61 @@ const Login = () => {
         </form>
         <ToastContainer />
       </div>
+      {/* <div className="w-full max-w-7xl mx-auto px-6 md:px-10 lg:px-20 bg-[#F5F5F5] font-poppins selection:bg-[#C2F578] selection:text-black">
+        <div className="h-[85vh] flex items-center justify-center">
+          <div className="bg-[#ECEDEF] flex flex-col items-center py-7 px-14 rounded-2xl">
+            <h1 className="text-xl font- text-center">Login</h1>
+            <form onSubmit={handelLoginSubmit} className="mt-6">
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Email
+                </label>
+                <input
+                  onChange={handelLoginChange}
+                  value={loginInfo.email}
+                  type="email"
+                  placeholder="Enter Email"
+                  name="email"
+                  autoFocus
+                  required
+                  className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-black"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Password
+                </label>
+                <input
+                  onChange={handelLoginChange}
+                  value={loginInfo.password}
+                  type="password"
+                  name="password"
+                  placeholder="Enter Email id..."
+                  autoFocus
+                  className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-black"
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full py-2 mt-4 text-black bg-[#c2f578] rounded-md hover:bg-[#a5cf66] transition-colors ease-linear hover:text-black"
+              >
+                Login
+              </button>
+            </form>
+            <span className="text-center text-sm pt-5">
+              Dont have an account?
+              <a
+                onClick={() => navigate("/singup")}
+                href=""
+                className="underline"
+              >
+                Sing Up
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <ToastContainer /> */}
     </>
   );
 };
