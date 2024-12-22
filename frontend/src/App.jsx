bd1e02a8e7b2671c4a36e4396771a9d5156898e3
unknown <yc0519165>
2024-12-22 14:12:01 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Singup from "./pages/Singup";
 
 function App() {
   return (
     <>
-      <div className=""></div>
+      <div className="">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/singup" element={<Singup />} />
+        </Routes>
+      </div>
     </>
   );
 }
